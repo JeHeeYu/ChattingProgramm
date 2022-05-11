9e7de340c70c0633382119ca621e2760a27da983
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-05-11 21:30:28 +0900

    Create server.h

---
+++ Server/server.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/file.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <time.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX_LENGTH      1024
+#define MAX_LINE        511
+#define START_STRING    "Start"
+#define END_STRING      "End"
+
+void AddClient(int s, struct sockaddr_in *newcliaddr);
+void RemoveClient(int s);
+void* MainThread(void* args);
